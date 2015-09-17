f22944e9a3ddb629f9c47d57b51524ed6eb4d686
Suhku Huh <nineclue@gmail.com>
2015-09-17 15:33:30 +0900

    basic drawing implemented

--- src/main/scala/Spirograph.scala
+++ src/main/scala/Spirograph.scala
@@ -6,7 +6,8 @@ import javafx.fxml.{FXML, FXMLLoader}
 import javafx.scene.layout.{HBox, VBox}
 import javafx.scene.Scene
 import javafx.scene.shape.Circle
-import javafx.scene.image.ImageView
+import javafx.scene.image.{ImageView, WritableImage}
+import javafx.scene.paint.Color
 
 object Spirograph {
 	val (width, height) = (800, 600)
@@ -14,6 +15,7 @@ object Spirograph {
 }
 
 class Spirograph extends Application {
+	// FXML stuffs
 	@FXML
 	var dImg:ImageView = _
 	@FXML
@@ -27,13 +29,50 @@ class Spirograph extends Application {
 	@FXML
 	var dotP:Circle = _
 
+	private def gcd(a:Int, b:Int):Int = if ((a % b) == 0) b else gcd(b, a%b)
+	private def mapMaker(minA:Double, maxA:Double, minB:Double, maxB:Double)(a:Double) =
+		(a - minA) * (maxB - minB) / (maxA - maxB) + minB
+
 	def start(ps:Stage) = {
 		var loader = new FXMLLoader(getClass.getResource("/main.fxml"))
 		loader.setController(this)
 		val root = loader.load.asInstanceOf[HBox]
 		root.setStyle("-fx-font-family: D2Coding; -fx-font-size: 16")
-		ps.setScene(new Scene(root, Spirograph.width, Spirograph.height))
-		ps.setTitle("Spirograph")
+
+		val mainScene = new Scene(root, Spirograph.width, Spirograph.height)
+		ps.setScene(mainScene)
+		ps.setTitle("Spirograph 스파이로그래프")
+		ps.setResizable(false)
+
+		initImage
 		ps.show
+	}
+
+	private val image = new WritableImage(600, 600)
+	def initImage() = {
+		dImg.setImage(image)
+		/*
+ 		val iWriter = image.getPixelWriter
+		val cMap = mapMaker(0, 600, 0, 360)_
+		Range(0, 600).foreach(i => {
+				iWriter.setColor(i, 300, Color.hsb(cMap(i), 1.0, 1.0))
+			})
+		*/
+		drawSpiro(image, 220, 65, 0.8)
+	}
+
+	def drawSpiro(im:WritableImage, outr:Double, inr:Double, ratio:Double) = {
+		def d2r(d:Double) = d * math.Pi / 180
+		val p = ratio * inr
+		val pCon = (outr - inr) / inr
+		val cd = gcd(outr.toInt, inr.toInt)
+		val rounds = (outr / cd) * 360
+		val writer = im.getPixelWriter
+		(0 to rounds.toInt).foreach(a => {
+			val r = d2r(a)
+			val x = (outr - inr) * math.cos(r) + p * math.cos(pCon*r)
+			val y = (outr - inr) * math.sin(r) - p * math.sin(pCon*r)
+			writer.setColor(x.toInt + 300, y.toInt + 300, Color.BLACK)
+			})
 	}
 }
